6c5bcfddd77bec0802d998504a3cec2e103ee68f
Lucas Alberto <lucas.alberto98@gmail.com>
2023-06-04 11:01:50 -0300

    fix: env url database

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -112,6 +112,7 @@ if IS_HEROKU_APP:
     # https://github.com/jazzband/dj-database-url
     DATABASES = {
         "default": dj_database_url.config(
+            default=os.environ.get("CLEARDB_DATABASE_URL"),
             conn_max_age=600,
             conn_health_checks=True,
             ssl_require=True,
